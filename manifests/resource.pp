3adf698fc2030f052476e864a90e3a03784d5eb7
Thomas Merkel <tm@core.io>
2013-04-05 18:27:32 +0200

    support multible disks

--- manifests/resource.pp
+++ manifests/resource.pp
@@ -1,11 +1,12 @@
 define drbd::resource (
   $hosts,
   $ips,
-  $disks,
+  $disk,
   $dev = '/dev/drbd0',
   $rate = '100M',
   $port = '7789',
   $metadisk = 'internal',
+  $options = undef
 ) {
 
   include drbd
@@ -13,5 +14,20 @@ define drbd::resource (
   file {"${drbd::config_pool}/${title}.res":
     ensure  => present,
     content => template('drbd/resource.erb'),
+  }
+
+  exec { "intialize DRBD metadata for $name":
+    command => "drbdadm create-md $name",
+    onlyif  => "test -e $disk",
+    unless  => "drbdadm dump-md $name || (drbdadm cstate $name | egrep -q '^(Sync|Connected)')",
+    before  => Service["drbd"],
+    require => File["${drbd::config_pool}/${title}.res"],
+  }
+
+  exec { "enable DRBD resource $name":
+    command => "drbdadm up $name",
+    onlyif  => "drbdadm dstate $name | egrep -q '^Diskless/|^Unconfigured'",
+    before  => Service["drbd"],
+    require => Exec["intialize DRBD metadata for $name"],
   }
 }
